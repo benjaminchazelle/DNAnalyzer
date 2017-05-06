6f4f4cee4531769f856c7e11cddc90f89b7e7dad
Egaro555 <hugues.vogel@hotmail.fr>
2017-05-06 16:59:07 +0200

    Revert "Revert "Ajout de methode de test (incomplete) de Dictionnaire""
    This reverts commit c0a5540b2f83139a18d18956eb7a59f5d78692ee.

--- DNAnalyzer/DNAnalyzerServerTest/DictionnaireTest.cpp
+++ DNAnalyzer/DNAnalyzerServerTest/DictionnaireTest.cpp
@@ -1,8 +1,11 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "../DNAnalyzerServer/Dictionnaire.h"
+#include "../DNAnalyzerServer/Mots.h"
 #include <exception>
+#include <unordered_set>
 #include <cstring>
+#include <string>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -22,6 +25,30 @@ namespace DNAnalyzerServerTest
 		{
 			// L'instance retournée doit toujours être la même
 			Assert::IsTrue(&(Dictionnaire::ObtenirInstance()) == &(Dictionnaire::ObtenirInstance()));
+		}
+		TEST_METHOD(ObtenirMaladie_KnownMaladie) {
+
+		}
+		TEST_METHOD(ObtenirMaladie_UnKnownMaladie) {
+
+		}
+		TEST_METHOD(ObtenirMaladies_NoMaladies) {
+			char motInexistant[] = "ATCGINEXISTANT";
+			unsigned int indexMotInexistant = Mots::ObtenirInstance().InsererMot(motInexistant);
+			const unordered_set<const Maladie*> resutat = Dictionnaire::ObtenirInstance().ObtenirMaladies(indexMotInexistant);
+			Assert::AreEqual(resutat.size, 0);
+		}
+		TEST_METHOD(ObtenirMaladies_OneMaladies) {
+
+		}
+		TEST_METHOD(ObtenirMaladies_TwoMaladies) {
+
+		}
+		TEST_METHOD(ObtenirNomMaladies_EmptyDictionnaire) {
+
+		}
+		TEST_METHOD(ObtenirNomMaladies_NotEmptyDictionnaire) {
+
 		}
 	};
 }
