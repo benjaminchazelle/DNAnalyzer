968baf2d24d3a8724df8e393ccf608bc8e92ca85
Egaro555 <hugues.vogel@hotmail.fr>
2017-05-09 00:30:23 +0200

    implentation du code CommunicationThread dans le header

--- DNAnalyzer/DNAnalyzerServer/CommunicationThread.cpp
+++ DNAnalyzer/DNAnalyzerServer/CommunicationThread.cpp
@@ -18,9 +18,109 @@ e-mail               :	hugues.vogel@insa-lyon.fr
 
 //----------------------------------------------------------------- PUBLIC
 
-
-CommunicationThread::CommunicationThread()
+void CommunicationThread::Traiter()
 {
+	/* EXAMPLE
+	cout << "Traiter" << endl;
+
+	string request("");
+
+	int iResult;
+	// Receive until the peer shuts down the connection
+	do {
+
+		const int recvbuflen = 512;
+		char recvbuf[recvbuflen + 1];
+
+
+
+		iResult = recv(*csock, recvbuf, recvbuflen, 0);
+		cout << "recv " << iResult << endl;
+
+		if (iResult > 0) {
+
+			
+			//char* sub = new char[iResult + 1];
+			//memcpy(sub, &recvbuf[recvbuflen], iResult);
+			//sub[iResult] = '\0';
+			
+
+
+			//cout << "iResult" << iResult << endl;
+			recvbuf[iResult] = '\0';
+
+			request += recvbuf;
+
+			cout << "nreq " << request << endl;
+
+			if (request.rfind("\r\n\r\n") != string::npos || request.rfind(";\r\n") != string::npos) {
+				Master::Handle(request, *this);
+
+				break;
+			}
+
+
+
+			//printf("Bytes received: %d\n", iResult);
+
+			// Echo the buffer back to the sender
+
+			//					cout << recvbuf;
+
+
+		}
+		else if (iResult == 0) {
+
+			cout << "pre handle" << endl;
+
+			Master::Handle(request, *this);
+
+			printf("Connection closing...\n");
+		}
+		else {
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			//closesocket(*csock);
+			//WSACleanup();
+			//return 1;
+			break;
+		}
+
+	} while (iResult > 0);
+
+	// shutdown the connection since we're done
+	iResult = shutdown(*csock, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(*csock);
+		WSACleanup();
+		//	return 1;
+	}
+	*/
+
+}
+
+
+void CommunicationThread::Repondre(const unsigned char[] data, const unsigned int longeur)
+{
+	/* EXAMPLE pour un paramétre (string msg)
+	int iSendResult = send(*csock, msg.c_str(), msg.length(), 0);
+	if (iSendResult == SOCKET_ERROR) {
+		printf("send failed with error: %d\n", WSAGetLastError());
+		closesocket(*csock);
+		WSACleanup();
+		//	return 1;
+	}
+	printf("Bytes sent: %d\n", iSendResult);
+	*/
+}
+
+
+CommunicationThread::CommunicationThread(SOCKET * csock)
+{
+	/* EXEMPLE
+	cout << "Thread inited" << endl;
+	Traiter();
+	*/
 }
 
 CommunicationThread::~CommunicationThread()
--- DNAnalyzer/DNAnalyzerServer/CommunicationThread.h
+++ DNAnalyzer/DNAnalyzerServer/CommunicationThread.h
@@ -13,6 +13,9 @@ e-mail               :	hugues.vogel@insa-lyon.fr
 
 //--------------------------------------------------- Interfaces utilisées
 
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
 using namespace std;
 
 
@@ -27,15 +30,25 @@ class CommunicationThread
 {
 protected:
 
+	SOCKET * csock;
+	//socket de la communication entre le serveur et le cleint
+
+	void Traiter();
+	// Methode de utiliser pour la resseption de la requette
+
 public:
+	void Repondre(const unsigned char data[],const unsigned int longeur);
+	// Permet de repondre a une requette d'un client
+
 	CommunicationThread & operator = (const CommunicationThread &);
 	// Mode d'emploi : opérateur d'affectation, non implementé
 
 	CommunicationThread(const CommunicationThread &);
 	// Mode d'emploi : constructeur de copie, non implementé
 
-	CommunicationThread();
+	CommunicationThread(SOCKET * csock);
 	// Mode d'emploi : constructeur
+
 
 	virtual ~CommunicationThread();
 };
