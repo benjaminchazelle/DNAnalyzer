d891ee4586faa8c4d22a4de87b89af0cd6ea84c8
Benjamin <Benjamin@DESKTOP-QUARKX>
2017-05-20 02:28:14 +0200

    Nouvelle architecture du traitement d'une requête

--- DNAnalyzer/DNAnalyzerServer/Master.cpp
+++ DNAnalyzer/DNAnalyzerServer/Master.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
-Master - Classe qui gére la communication entre un clientInfo et le service demander 
+Master - Classe qui gére la communication entre un clientInfo et le service demander
 -------------------
 début                :	06/05/17
 copyright            :	(C) 2017 par VOGEL
@@ -17,12 +17,56 @@ e-mail               :	hugues.vogel@insa-lyon.fr
 
 #include "Master.h"
 #include "CommunicationThread.h"
+#include "Dictionnaire.h"
+#include "Analyse.h"
+
+#define UNREFERENCE_PARAMETER(P) (P)
+
 
 //----------------------------------------------------------------- PUBLIC
 
 void Master::InterpreterRequete(CommunicationThread & thread)
 {
-	//TODO : Change
+	if (thread.LireLigne() != "MA v1.0") {
+		repondreErreurRequete("Invalid syntax", thread);
+		thread.FermerConnexion();
+		return;
+	}
+
+	string serviceName = thread.LireLigne();
+
+	if (serviceName == "CHECK DISEASE")
+	{
+		string maladie = thread.LireLigne();
+
+		string genome = thread.LireLigne();
+
+		analysePrecise(maladie, genome, thread);
+	}
+	else if (serviceName == "CHECK ALL")
+	{
+		string genome = thread.LireLigne();
+
+		analyseGlobale(genome, thread);
+	}
+	else if (serviceName == "GET DISEASES")
+	{
+		string crlf = thread.LireLigne();
+
+		if (crlf == "") {
+			obtenirListeMaladies(thread);
+		}
+		else {
+			repondreErreurRequete("Invalid syntax", thread);
+		}
+	}
+	else
+	{
+		repondreErreurRequete("Unknown service", thread);
+	}
+
+	thread.FermerConnexion();
+
 }
 
 //----------------------------------------------------------------- PRIVEE
@@ -32,22 +76,69 @@ Route Master::routerRequete(const string & serviceLigne)
 	return Route();
 }
 
-void Master::analysePrecise(const string & maladie, unordered_set<string> & genome, CommunicationThread & thread)
+void Master::analysePrecise(const string & nomMaladie, const string & genome, CommunicationThread & thread)
 {
+	string response = "MA v1.0\r\n";
+
+	try {
+
+		Maladie maladie = Dictionnaire::ObtenirInstance().ObtenirMaladie(nomMaladie);
+		
+		bool result = Analyse::AnalysePrecise(genome, maladie);
+		
+		response += "DESEASE " + nomMaladie;
+		response += " ";
+		response += result ? "1" : "0";
+		response += "\r\n\r\n";
+
+		thread.Repondre(response);
+	}
+	catch (range_error const& e) {
+		UNREFERENCE_PARAMETER(e);
+
+		repondreErreurRequete("Unknown disease", thread);
+
+	}
+
 }
 
-void Master::analyseGlobale(const unordered_set<string> & genome, CommunicationThread & thread)
+void Master::analyseGlobale(const string & genome, CommunicationThread & thread)
 {
+	string response = "MA v1.0\r\n";
+
+	for (const auto& resultatMaladie : Analyse::AnalyseGlobale(genome)) {
+		if (resultatMaladie.second) {
+			response += "DESEASE ";
+			response += resultatMaladie.first.nom + "\r\n";
+		}
+
+	}
+
+	response += "\r\n";
+
+	thread.Repondre(response);
 }
 
 void Master::obtenirListeMaladies(CommunicationThread & thread)
 {
+	string response = "MA v1.0\r\n";
+
+	response += "DESEASES\r\n";
+
+	for (const auto& maladie : Dictionnaire::ObtenirInstance().ObtenirNomsMaladies()) {
+		response += maladie + "\r\n";
+	}
+
+	response += "\r\n";
+
+	thread.Repondre(response);
 }
 
-void Master::repondreServiceInconnu(const string & serviceName, CommunicationThread & thread)
+void Master::repondreErreurRequete(const string & error, CommunicationThread & thread)
 {
-}
+	string response = "MA v1.0\r\n";
+	response += "ERROR " + error;
+	response += "\r\n\r\n";
 
-void Master::repondreErreurRequete(const string & textes, CommunicationThread & thread)
-{
+	thread.Repondre(response);
 }
